b4c8fa2519adc7efeeafaed595d4fcd869dfc8d0
literate <gaekoon@gmail.com>
2022-07-09 10:10:47 +0900

    optimizing component with React.memo

--- src/component/Todos.js
+++ src/component/Todos.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function TodoItem({ todo, onToggle }) {
+const TodoItem = React.memo(function TodoItem({ todo, onToggle }) {
   return (
     <li
       style={{ textDecoration: todo.done ? 'line-through' : 'none' }}
@@ -11,9 +11,9 @@ function TodoItem({ todo, onToggle }) {
       {todo.text}
     </li>
   );
-}
+});
 
-function TodoList({ todos, onToggle }) {
+const TodoList = React.memo(function TodoList({ todos, onToggle }) {
   return (
     <ul>
       {todos.map((todo) => (
@@ -21,7 +21,7 @@ function TodoList({ todos, onToggle }) {
       ))}
     </ul>
   );
-}
+});
 
 export default function Todos({ todos, onCreate, onToggle }) {
   const [text, setText] = useState('');
